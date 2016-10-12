aae9666c1d65219058ebc575c6377b63c55c0f59
Grant Wells <gwells4@cnm.edu>
2016-10-12 11:33:39 -0600

    Outlined the three sections: persona, use case, and entities.

---
+++ public_html/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8" />
+		<link rel="Browser Tab Icon" href="img/icon-php-file.jpg" type="image/jpg">
+
+		<!-- Add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+
+		<title>Repository 4: Data Design - Craigslist Events</title>
+
+	</head>
+
+	<body>
+
+		<!-- This is the header -->
+		<header>
+			<h1>Data Design: Craigslist Events</h1>
+		</header>
+
+		<!-- This is the main page content. -->
+		<main>
+
+			<!-- Section 1: Persona -->
+			<section>
+				<h2>Persona</h2>
+				<p>Knausgaard chia helvetica post-ironic, meggings schlitz lomo.</p>
+				<img src="img/sandbox-image-1.jpg" alt="Picture for Persona">
+
+				<ol>
+					<li>
+						<b>Description</b><br>
+
+					</li>
+				</ol>A description gives a brief summary of the persona, which often contains demographic information (sometimes going into surprisingly large amounts of detail). The description might include (among other information) their job, their technology, any pressures they're under and the situation they're in when they use the system/site.
+				Needs, goals & features can either be a straight list for each persona, or a table comparing each persona against each possible need/feature/goal. This can help define a priority list for features (or the needs from which to develop features) before development starts.
+
+				Personas may contain a whole variety of other pieces of information, including:
+
+				A 'quote' from the persona
+				This should summarise their needs/goals into a single sentence. This can be a double edged sword, as this single sentence runs the risk of oversimplifying the personas issues. Quotes do have the added advantage of giving the personas further personality.
+				Frustrations
+				These are issues that the persona encounters, either with the existing system or because there's no existing system. These can clarify to designers/developers exactly what the biggest issues this persona faces.
+				Ideal features
+				This is very similar to features, but with no restrictions placed on their feasibility... these may be impossible to produce, but sometimes help designers/developers think of alternative approaches that are more realistic.
+				'Need to know'
+				Any information the persona needs when trying to do their goal. These help designers/developers understand the key information needed to communicate to users.
+				Behaviours
+				These are the typical behaviours the persona exhibits. They're usually only shown if the personas different behaviours lead to different needs/requirements of the system in development.
+				Scenarios
+				These are sample use cases the persona may do. They can be based on either their goals or their use of an existing system. This can clarify exactly how a system will be/is used.
+
+			</section>
+
+			<!-- Section 2: Use Case -->
+			<section>
+				<h2>Use Case</h2>
+				<ul>
+					<li>Senator Arlo</li>
+					<li>Star Wars</li>
+					<li>Donuts</li>
+				</ul>
+			</section>
+
+			<!-- Section 3: Entities -->
+			<section>
+				<h2>Entities</h2>
+				<table>
+
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+
+					<tr>
+						<td>Star Wars</td>
+						<td>Star Trek</td>
+					</tr>
+
+				</table>
+
+			</section>
+
+		</main>
+
+	</body>
+</html>
