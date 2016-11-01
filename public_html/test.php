e41d4208397293a5acb2a4aa8df578c474a14063
Grant Wells <gwells4@cnm.edu>
2016-11-01 15:52:34 -0600

    Updated test.php 2

--- public_html/test.php
+++ public_html/test.php
@@ -18,8 +18,46 @@ echo "</pre>";
 require_once "aes256.php";
 require_once "encrypted-config.php";
 
-
+$aes_file="aes49.ini";
 $arlo_file="arlo-control-center.ini";
+
+
+$filename=$aes_file;
+//$filename=$arlo_file;
+
+echo "<pre>";
+print_r($filename);
+echo "</pre>";
+
+
+// verify the file is readable
+if(is_readable($filename) === false) {
+	throw(new InvalidArgumentException("configuration file is not readable"));
+}
+
+// read the encrypted config file
+if(($ciphertext = file_get_contents($filename)) == false) {
+	throw(new InvalidArgumentException("unable to read configuration file"));
+}
+
+// decrypt the file
+try {
+	$password = strrev($filename) . strlen($filename);
+	$plaintext = aes256Decrypt($ciphertext, $password);
+} catch(InvalidArgumentException $invalidArgument) {
+	throw(new InvalidArgumentException($invalidArgument->getMessage(), 0 , $invalidArgument));
+}
+
+// parse the parameters and return them
+if(($parameters = parse_ini_string($plaintext)) === false) {
+	throw(new InvalidArgumentException("unable to parse parameters"));
+}
+
+
+
+
+
+
 $arlo_parameters=readConfig($arlo_file);
 
 
